818cd7b0a72b08148dc2cbe13e72bbbf8c77887c
Vitalii Strohush <vitalii.strohush@anvileight.com>
2024-01-03 21:59:58 +0200

    Post-api

--- client/src/redux/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { userInfoReducer } from './userInfo/slice';
-
-export const rootReducer = combineReducers({
-  userInfo: userInfoReducer,
-});
